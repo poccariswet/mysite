fc732023fc86988b8211d77deaf5719089c6f048
soeyu <s1240231@gmail.com>
2018-11-10 22:18:25 +0900

    [ADD] links component (#2)

--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,19 @@
       </h1>
       <h2 class="subtitle">
       </h2>
-      <div class="links">
-        <a
-          href="https://www.facebook.com/yusuke.soeta.92"
-          target="_blank"
-          class="button--facebook">Facebook</a>
-        <a
-          href="https://github.com/poccariswet"
-          target="_blank"
-          class="button--github">GitHub</a>
-        <a
-          href="https://speakerdeck.com/soeyusuke"
-          target="_blank"
-          class="button--speakerdeck">Speaker Deck</a>
-      </div>
+      <LinkButtons/>
     </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import LinkButtons from '~/components/LinkButtons.vue'
 
 export default {
   components: {
-    AppLogo
+    AppLogo,
+    LinkButtons
   }
 }
 </script>
@@ -60,51 +49,4 @@ export default {
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
-.links {
-  padding-top: 15px;
-}
-
-.button--facebook {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #333EA2;
-  color: #333EA2;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--facebook:hover {
-  color: #fff;
-  background-color: #333EA2;
-}
-
-.button--speakerdeck {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--speakerdeck:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--github {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--github:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-
 </style>
