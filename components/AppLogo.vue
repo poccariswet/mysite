1fda666e90f6ae0faf39157b9b1e51f4ffdf121d
poccariswet <s1240231@gmail.com>
2018-11-10 18:18:29 +0900

    first dev

---
+++ components/AppLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="poccariswetLogo">
+    <img class="image-icon" src="~/assets/997537.jpg">
+  </div>
+</template>
+
+<style>
+.image-icon {
+  height: 160px;
+  width: 160px;
+  border: solid 5px #fff;
+  border-radius: 50%;
+  box-shadow: 0 2px 5px 1px #242424;
+}
+</style>
+
