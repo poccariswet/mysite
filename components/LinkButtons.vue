301569d16159a90693d9258e5d5ccefc1b3c582d
soeyu <s1240231@gmail.com>
2018-11-10 22:44:44 +0900

    Develop (#3)
    * first dev
    
    * [ADD] links component (#2)

---
+++ components/LinkButtons.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class=links>
+    <a
+      href="https://www.facebook.com/yusuke.soeta.92"
+      target="_blank"
+      class="button--facebook">Facebook</a>
+    <a
+      href="https://github.com/poccariswet"
+      target="_blank"
+      class="button--github">GitHub</a>
+    <a
+      href="https://speakerdeck.com/soeyusuke"
+      target="_blank"
+      class="button--speakerdeck">Speaker Deck</a>
+  </div>
+</template>
+
+<style>
+.links {
+  padding-top: 15px;
+}
+
+.button--facebook {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #333EA2;
+  color: #333EA2;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--facebook:hover {
+  color: #fff;
+  background-color: #333EA2;
+}
+
+.button--speakerdeck {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--speakerdeck:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--github {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--github:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
