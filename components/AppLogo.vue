49b1764fe1662e5f8be33670ceddf31b014f7c86
soeyu <s1240231@gmail.com>
2018-11-11 18:31:03 +0900

    Develop (#5)
    * first dev
    
    * [ADD] links component (#2)
    
    * ui animation and css (#4)
    
    ui animation and scss

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -11,6 +11,23 @@
   border: solid 5px #fff;
   border-radius: 50%;
   box-shadow: 0 2px 5px 1px #242424;
+  animation: turn 2.5s ease-in;
+}
+
+@keyframes turn {
+  0% {
+    transform: rotate(-360deg);
+  }
+  50% {
+    width: 300px;
+    height: 300px;
+  }
+  80% {
+    transform: rotate(50deg)
+  }
+  100% {
+    transform: rotate(0);
+  }
 }
 </style>
 
