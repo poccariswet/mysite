086dcb2712d2c1144bf48c629d4a0bfe99730636
soeyu <s1240231@gmail.com>
2018-11-11 18:29:44 +0900

    ui animation and css (#4)
    ui animation and scss

--- components/LinkButtons.vue
+++ components/LinkButtons.vue
@@ -15,50 +15,44 @@
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 .links {
   padding-top: 15px;
-}
-
-.button--facebook {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #333EA2;
-  color: #333EA2;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--facebook:hover {
-  color: #fff;
-  background-color: #333EA2;
-}
-
-.button--speakerdeck {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--speakerdeck:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--github {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--github:hover {
-  color: #fff;
-  background-color: #35495e;
+  .button--facebook {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #333EA2;
+    color: #333EA2;
+    text-decoration: none;
+    padding: 10px 30px;
+    &:hover {
+      color: #fff;
+      background-color: #333EA2;
+    }
+  }
+  .button--speakerdeck {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+    &:hover {
+      color: #fff;
+      background-color: #3b8070;
+    }
+  }
+  .button--github {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    &:hover {
+      color: #fff;
+      background-color: #35495e;
+    }
+  }
 }
 </style>
