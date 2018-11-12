985772b00d8e454b299bedea8079114c2ee13cbd
poccariswet <s1240231@gmail.com>
2018-11-12 19:54:01 +0900

    [ADD] username effect

---
+++ components/Overview.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="overview">
+    <div class="username" data-text="poccariswet">
+      poccariswet
+    </div>
+    <h2 class="subtitle">
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.username{
+  color:grey;
+  font-size:100px;
+  position:relative;
+  margin:auto;
+}
+
+@keyframes noise-anim{
+  $steps:20;
+  @for $i from 0 through $steps{
+    #{percentage($i*(1/$steps))}{
+      clip:rect(random(100)+px,9999px,random(100)+px,0);
+    }
+  }
+}
+.username:after{
+  content:attr(data-text);
+  position:absolute;
+  left:2px;
+  text-shadow:-1px 0 red;
+  top:0;
+  color:grey;
+  background:white;
+  overflow:hidden;
+  clip:rect(0,900px,0,0);
+  animation:noise-anim 2s infinite linear alternate-reverse;
+}
+
+@keyframes noise-anim-2{
+  $steps:20;
+  @for $i from 0 through $steps{
+    #{percentage($i*(1/$steps))}{
+      clip:rect(random(100)+px,9999px,random(100)+px,0);
+    }
+  }
+}
+.username:before{
+  content:attr(data-text);
+  position:absolute;
+  left:-2px;
+  text-shadow:1px 0 blue;
+  top:0;
+  color:grey;
+  background:white;
+  overflow:hidden;
+  clip:rect(0,900px,0,0);
+  animation:noise-anim-2 3s infinite linear alternate-reverse;
+}
+</style>
