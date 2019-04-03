56faf37b799de8efa5f917c4a7f5e8f0441a2a84
poccariswet <s1240231@gmail.com>
2019-04-03 16:21:41 +0900

    [ADD] #16 icons glitch

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,14 +1,74 @@
 <template>
-  <div class="poccariswetLogo">
-    <img class="image-icon" src="~/assets/997537.jpg">
+  <div class="glitch-image">
+    <div class="glitch">
+      <div class="glitch-inner">
+        <div class="glitch-inner-2">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-.image-icon {
-  height: 300px;
-  width: 300px;
-  border: solid 5px #fff;
+<style lang="scss" scoped>
+@keyframes noise-anim{
+  $steps: 20;
+  @for $i from 0 through $steps{
+    #{percentage($i*(1/$steps))}{
+     $r: random(500);
+     clip:rect( $r +px,9999px, $r + random(100)+px, 0);
+     left: random(10) - 5%;
+     filter: saturate(random(100)/5);
+     transform: scale(1 + random(3)/10);
+    }
+  }
+}
+
+@keyframes noise-anim-2{
+  $steps: 20;
+  @for $i from 0 through $steps{
+    #{percentage($i*(1/$steps))}{
+     $k: random(500);
+     clip:rect( $k +px,9999px, $k + random(50)+px, 0);
+     left: random(10) - 5%;
+     transform: scale(1 + random(3)/10);
+     filter: saturate(random(100)/5);
+    }
+  }
+}
+
+.glitch-image {
+  display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+.glitch, .glitch-inner, .glitch-inner-2 {
+ width: 300px;
+ height: 300px;
+ background-image: url(https://avatars1.githubusercontent.com/u/24663217?s=400&u=106ed535246c47bf666ed9e59ec15f6b7399e3c7&v=4);
+ background-size: cover;
+}
+
+.glitch {
+ position: relative;
+ overflow: hidden;
+}
+.glitch-inner {
+ position: absolute;
+ top: 0;
+ left: 0;
+ opacity: .5;
+ clip: rect(60px, 9999px, 60px, 0);
+ animation: noise-anim 3s infinite step-end;
+}
+
+.glitch-inner-2 {
+ position: absolute;
+ top: 0;
+ left: 0;
+ opacity: .5;
+ clip: rect(60px, 9999px, 60px, 0);
+ animation: noise-anim-2 3s infinite step-end;
 }
 </style>
-
