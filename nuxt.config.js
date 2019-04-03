f10bae659659abe7f310f2c268b428c46f367cf8
poccariswet <s1240231@gmail.com>
2019-04-03 13:58:43 +0900

    [FIX] bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,14 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
-  /*
+   /*
   ** Build configuration
   */
   build: {
