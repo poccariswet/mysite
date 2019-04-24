dafe7dbe0861b702bb74b38255273e537c51e0a2
poccariswet <s1240231@gmail.com>
2019-04-24 14:12:03 +0900

    [ADD] button

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@
         <app-logo/>
         <Overview style="margin-top:2em;" />
         <LinkButtons/>
-        <iframe width="640" height="360" src="https://www.youtube.com/embed/GegJz41l5Q8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </section>
   </div>
