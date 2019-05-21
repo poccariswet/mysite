850ba6ba0b83030a988a541741019d4e842bfd22
soe <s07257@10015659pc.cyberagent.co.jp>
2019-05-21 18:03:03 +0900

    [FIX]

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyAfCfPkSslya9YpiH71H-hcWlxzqtGKdcs",
-  authDomain: "my-site-poccariswet.firebaseapp.com",
-  databaseURL: "https://my-site-poccariswet.firebaseio.com",
-  projectId: "my-site-poccariswet",
-  storageBucket: "my-site-poccariswet.appspot.com",
-  messagingSenderId: "1046438060461"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 }
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
